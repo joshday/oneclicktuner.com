f95644ea60da3095315b91720f31d4704bcdb7f2
Josh Day <emailjoshday@gmail.com>
2022-05-23 13:24:51 -0400

    update reviews

--- app/src/components/Reviews.js
+++ app/src/components/Reviews.js
@@ -60,7 +60,7 @@ export default function Example() {
                     <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
                       <div className="relative text-lg font-medium text-white md:flex-grow">
                         <QuoteSVG />
-                        <p className="relative text-align-center">
+                        <p className="relative font-light text-align-center">
                           {person.quote}
                         </p>
                       </div>
@@ -77,108 +77,3 @@ export default function Example() {
     </div>
   )
 }
-
-
-
-
-// //-----------------------------------------------------------------------------// Review
-// const Review = ({img, quote, attribution}) => (
-//   <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border md:border-gray-900 lg:pr-16">
-//     <div className="md:flex-shrink-0">
-//       {img}
-//     </div>
-//     <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
-//       <div className="relative text-lg font-medium text-white md:flex-grow">
-//         <QuoteSVG />
-//         <p className="relative text-align-center">
-//           {quote}
-//         </p>
-//       </div>
-//     </blockquote>
-//     <footer className="mt-8">
-//       <div className="flex items-start">
-//         <div className="ml-4">
-//           <div className="text-base font-medium text-gray-200">{attribution}</div>
-//         </div>
-//       </div>
-//     </footer>
-//   </div>
-// )
-
-// //-----------------------------------------------------------------------------// Reviews
-// export default function Reviews() {
-//   return (
-//     <section className="bg-gray-800 px-16">
-//       <div className="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
-//         <Review
-//           img={(
-//             <A href="https://www.theguitarjournal.com/one-click-tuner-minimalist-guitar-tuner-app-frequency-display/">
-//               <img className="h-24 w-24 rounded-full mx-auto" src="images/guitarjournal.jpg" alt="guitar journal logo" />
-//             </A>
-//           )}
-//           quote = "If you’re a minimalist, and you want the most stripped down and effective tuner app available, this is it."
-//         />
-
-//         <Review
-//           img={(
-//             <A href="https://producthunt.com/posts/one-click-tuner">
-//               <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=153658&amp;theme=light" alt="One Click Tuner - Innovative Musical Instrument Tuner for iOS | Product Hunt Embed" className="h-16 mx-auto" />
-//             </A>
-//           )}
-//           quote=
-//           attribution="ProductHunt user"
-//         />
-//       </div>
-
-
-//       <div className="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
-//         <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-gray-900 lg:pr-16">
-//           <div className="md:flex-shrink-0">
-//             <A href="https://www.theguitarjournal.com/one-click-tuner-minimalist-guitar-tuner-app-frequency-display/">
-//               <img className="h-24 w-24 rounded-full mx-auto" src="images/guitarjournal.jpg" alt="guitar journal logo" />
-//             </A>
-//           </div>
-//           <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
-//             <div className="relative text-lg font-medium text-white md:flex-grow">
-//               <QuoteSVG />
-//               <p className="relative">
-//                 I'd like to take the opportunity to again express my appreciation for the One Click app. Its simple answer to a common real-world request is uncommonly elegant amongst the noisy offerings of the current digital climate. Well done.
-//               </p>
-//             </div>
-//           </blockquote>
-//         </div>
-
-//         <div className="py-12 px-4 border-t-2 border-gray-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
-//           <div className="md:flex-shrink-0">
-//             <A href="https://producthunt.com/posts/one-click-tuner">
-//               <img src="images/user.png" alt="user logo" className="h-16 mx-auto invert" />
-//             </A>
-//           </div>
-//           <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
-//             <div className="relative text-lg font-medium text-white md:flex-grow">
-//               <QuoteSVG />
-//               <p className="relative">
-//               I'd like to take the opportunity to again express my appreciation for the One Click app. Its simple answer to a common real-world request is uncommonly elegant amongst the noisy offerings of the current digital climate. Well done.
-//               </p>
-//             </div>
-//           </blockquote>
-//           <footer className="mt-8">
-//               <div className="flex items-start">
-//                 {/* <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-//                   <img
-//                     className="h-12 w-12 rounded-full"
-//                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-//                     alt=""
-//                   />
-//                 </div> */}
-//                 <div className="ml-4">
-//                   {/* <div className="text-base font-medium text-white">Joel Sutton</div> */}
-//                   <div className="text-base font-medium text-gray-200">Product Hunt Review</div>
-//                 </div>
-//               </div>
-//             </footer>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
